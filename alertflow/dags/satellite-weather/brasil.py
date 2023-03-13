5fb2abb046e02081f4ce0095fce22ab4ba200857
Luã Bida Vacaro <luabidaa@gmail.com>
2023-03-13 20:15:36 -0300

    Trying to finish satellite DAG

--- alertflow/dags/satellite-weather/brasil.py
+++ alertflow/dags/satellite-weather/brasil.py
@@ -1,19 +1,18 @@
-import asyncio
 import os
-from datetime import datetime, timedelta
-from pathlib import Path, PosixPath
-
-import asyncpg
-import pandas as pd
 import pendulum
-import satellite_downloader as sat_d
+
+from dateutil import parser
+from pathlib import Path, PosixPath
+from sqlalchemy import create_engine
+from datetime import datetime, timedelta
+
 import satellite_weather as sat_w
+import satellite_downloader as sat_d
+from satellite_weather._brazil.extract_latlons import MUNICIPIOS
 
 from airflow import DAG
-from airflow.decorators import dag, task
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import BranchPythonOperator
-from satellite_weather._brazil.extract_latlons import MUNICIPIOS
+from airflow.decorators.python import python_task
+from airflow.operators.python import PythonOperator
 
 env = os.getenv
 email_main = env('EMAIL_MAIN')
@@ -21,13 +20,15 @@ DATA_DIR = '/tmp/copernicus'
 DEFAULT_ARGS = {
     'owner': 'AlertaDengue',
     'depends_on_past': False,
-    'email': [email_main],
+    # 'email': [email_main],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 2,
     'retry_delay': timedelta(minutes=2),
-    'catchup': True,
 }
+
+UID = env('CDSAPI_UID')
+KEY = env('CDSAPI_KEY')
 
 PG_URI_MAIN = (
     'postgresql://'
@@ -39,112 +40,60 @@ PG_URI_MAIN = (
 )
 
 
-
-
-## Dags
-### Brasil
-@dag(
+with DAG(
     dag_id='COPERNICUS_BRASIL',
     description='ETL of weather data for Brazil',
     tags=['Brasil', 'Copernicus'],
-    schedule_interval='@daily',
-    start_date=pendulum.datetime(2000, 1, 1),
+    schedule='@daily',
     default_args=DEFAULT_ARGS,
-)
-def brasil():
-    ## Tasks
-    @task(task_id='start')
-    def initial_task():
-        exec_date = '{{ ds }}'
-        Path(DATA_DIR).mkdir(exist_ok=True)
-        return datetime.date(exec_date)
+    start_date=pendulum.datetime(2000, 1, 1),
+    catchup=False,
+):
 
-
-    def _is_date_available(date: datetime.date) -> bool:
-        max_data_delay = datetime.date(datetime.now() - timedelta(days=7))
-        return 'yes' if date < max_data_delay else 'no'
-
-
-    check_date = BranchPythonOperator(
-        task_id='is_date_available',
-        python_callable=_is_date_available,
-        op_kwargs={'date': '{{ ds }}'},
-    )
-
-    proceed = EmptyOperator(task_id='yes')
-
-    stop = EmptyOperator(task_id='no')
-
-
-    @task(task_id='extract')
-    def download_netcdf(
-        data_days_interval: int = None, data_dir=DATA_DIR, **kwargs
-    ) -> PosixPath:
-        ti = kwargs['ti']
-        ini_date = ti.xcom_pull(task_ids='start')
-
-        if not data_days_interval:
-            end_date = None
-        else:
-            end_date = datetime.date(ini_date - timedelta(days=data_days_interval))
+    def download_netcdf(ini_date: str) -> PosixPath:
+        start_date = parser.parse(str(ini_date)).date()
+        max_update_delay = start_date - timedelta(days=9)
 
         try:
             netcdf_file = sat_d.download_br_netcdf(
-                date=ini_date, end_date=end_date, data_dir=data_dir
+                date=str(max_update_delay),
+                date_end=None,
+                data_dir=DATA_DIR,
+                uid=UID,
+                key=KEY,
             )
-            return Path(data_dir) / netcdf_file
+            filepath = Path(DATA_DIR) / netcdf_file
+            return filepath
         except Exception as e:
             raise e
 
-
-    async def _to_sql(dataframe: pd.DataFrame, tablename: str, timeout=None):
-        connection = await asyncpg.connect(dsn=PG_URI_MAIN)
-        insert = await connection.copy_records_to_table(
-            tablename,
-            records=dataframe.values.tolist(),
-            columns=dataframe.columns,
-            schema_name='weather',
-            timeout=timeout,
-        )
-        await connection.close()
-        return insert
-
-
-    @task(task_id='clean')
-    def delete_netcdf(**kwargs):
-        ti = kwargs['ti']
-        netcdf_file = ti.xcom_pull(task_ids='extract')
-        Path(netcdf_file).unlink(missing_ok=False)
-
-
-    done = EmptyOperator(
-        task_id='done',
-        trigger_rule='one_success',
+    E = PythonOperator(
+        task_id='extract',
+        python_callable=download_netcdf,
+        op_kwargs={'ini_date': '{{ execution_date.strftime("%Y-%m-%d") }}'},
     )
 
-
-    async def _load_and_insert(file):
-        dataset = sat_w.load_dataset(file)
+    @python_task(task_id='loading')
+    def upload_dataset(**context) -> PosixPath:
+        ti = context['ti']
+        file = ti.xcom_pull(task_ids='extract')
+        ds = sat_w.load_dataset(file)
         for mun in MUNICIPIOS:
-            df = dataset.copebr.to_dataframe(mun['geocodigo'], raw=True)
-            insert = await _to_sql(df, 'copernicus_brasil')
-            return insert
+            df = ds.copebr.to_dataframe(mun['geocodigo'], raw=True)
+            df.to_sql(
+                name='copernicus_brasil',
+                schema='weather',
+                con=create_engine(PG_URI_MAIN),
+            )
+        return file
 
-    @task(task_id='loading')
-    def loading(**kwargs):
-        ti = kwargs['ti']
-        netcdf_file = ti.xcom_pull(task_ids='extract')
-        loop = asyncio.get_event_loop()
-        res = loop.run_until_complete(_load_and_insert(netcdf_file))
-        return res
+    @python_task(task_id='clean')
+    def remove_netcdf(**context):
+        ti = context['ti']
+        file = ti.xcom_pull(task_ids='extract')
+        Path(file).unlink(missing_ok=False)
 
-    start = initial_task()
-    E = download_netcdf()
-    L = loading()
-    clean = delete_netcdf()
+    TL = upload_dataset()
+    clean = remove_netcdf()
 
-    start >> check_date
-    check_date >> stop >> done
-    check_date >> proceed >> E >> L >> clean >> done
-
-dag = brasil()
+    E >> TL >> clean
