ca5f098442bac0cac17c4be3591747ee96ac62f0
Luã Bida Vacaro <luabidaa@gmail.com>
2023-03-16 13:41:38 -0300

    feat(satellite): copernicus satellite DAGS (#8)
    * feat(satellite): copernicus satellite DAGS
    
    * feat(satellite): copernicus satellite DAGS
    
    * Minor adjustments
    
    * Fix timezone
    
    * Trying to finish satellite DAG
    
    * Tasks
    
    * Finishing DAGs, implementing satellite changes
    
    * Append if exists
    
    * Linter

--- alertflow/dags/satellite-weather/foz_do_iguacu.py
+++ alertflow/dags/satellite-weather/foz_do_iguacu.py
@@ -1,5 +1,6 @@
+import calendar
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 from pathlib import Path, PosixPath
 
 import pendulum
@@ -9,12 +10,11 @@ from airflow.operators.python import PythonOperator
 from dateutil import parser
 from satellite import downloader as sat_d
 from satellite import weather as sat_w
-from satellite.weather._brazil.extract_latlons import MUNICIPIOS
 from sqlalchemy import create_engine
 
 env = os.getenv
 email_main = env('EMAIL_MAIN')
-DATA_DIR = '/tmp/copernicus'
+DATA_DIR = '/tmp/copernicus/foz'
 DEFAULT_ARGS = {
     'owner': 'AlertaDengue',
     'depends_on_past': False,
@@ -25,7 +25,6 @@ DEFAULT_ARGS = {
     'retry_delay': timedelta(minutes=2),
 }
 
-
 PG_URI_MAIN = (
     'postgresql://'
     f"{env('PSQL_USER_MAIN')}"
@@ -34,32 +33,54 @@ PG_URI_MAIN = (
     f":{env('PSQL_PORT_MAIN')}"
     f"/{env('PSQL_DB_MAIN')}"
 )
-TABLE_NAME = 'copernicus_brasil'
+TABLE_NAME = 'copernicus_foz_do_iguacu'
 SCHEMA = 'weather'
 
 
 with DAG(
-    dag_id='COPERNICUS_BRASIL',
-    description='ETL of weather data for Brazil',
-    tags=['Brasil', 'Copernicus'],
-    schedule='@daily',
+    dag_id='COPERNICUS_FOZ_DO_IGUACU',
+    description='ETL of weather data for Foz do Iguaçu',
+    tags=['Brasil', 'Copernicus', 'Foz do Iguaçu'],
+    schedule='@monthly',
     default_args=DEFAULT_ARGS,
-    start_date=pendulum.datetime(2000, 1, 9),
+    start_date=pendulum.datetime(2000, 2, 15),
     catchup=True,
 ):
 
     def download_netcdf(ini_date: str) -> PosixPath:
         """
         Downloads the file for current task execution
-        date - 9 days for safety reasons. Returns the
-        local NetCDF4 file to be inserted into postgres.
+        date - 1 month, extracting always the last month of
+        a given execution date. Returns the local NetCDF4
+        file to be inserted into postgres.
         """
-        start_date = parser.parse(str(ini_date)).date()
-        max_update_delay = start_date - timedelta(days=9)
+        start_date = parser.parse(str(ini_date))
+        if start_date.month == 1:   # If January
+            last_month = datetime(
+                year=start_date.year - 1, month=12, day=start_date.day
+            )
+        else:
+            last_month = datetime(
+                year=start_date.year,
+                month=start_date.month - 1,
+                day=start_date.day,
+            )
+
+        ini_date = datetime(
+            year=last_month.year, month=last_month.month, day=1
+        ).date()
+
+        _, last_month_day = calendar.monthrange(
+            year=last_month.year, month=last_month.month
+        )
+
+        end_date = datetime(
+            year=last_month.year, month=last_month.month, day=last_month_day
+        ).date()
 
         try:
             netcdf_file = sat_d.download_br_netcdf(
-                date=str(max_update_delay), data_dir=DATA_DIR
+                date=str(ini_date), date_end=str(end_date), data_dir=DATA_DIR
             )
             filepath = Path(DATA_DIR) / netcdf_file
             return str(filepath.absolute())
@@ -83,17 +104,13 @@ with DAG(
         ti = context['ti']
         file = ti.xcom_pull(task_ids='extract')
         ds = sat_w.load_dataset(file)
-        geocodes = [mun['geocodigo'] for mun in MUNICIPIOS]
-
-        for geocode in geocodes:
-            df = ds.copebr.to_dataframe(geocodes=geocode, raw=False)
-            with create_engine(PG_URI_MAIN).connect() as conn:
-                df.to_sql(
-                    name=TABLE_NAME,
-                    schema=SCHEMA,
-                    con=conn,
-                    if_exists='append',
-                )
+        df = ds.copebr.to_dataframe(geocodes=4108304, raw=True)
+        with create_engine(PG_URI_MAIN).connect() as conn:
+            df.to_sql(
+                name=TABLE_NAME,
+                schema=SCHEMA,
+                con=conn,
+            )
 
     @python_task(task_id='clean')
     def remove_netcdf(**context):
--- env.tpl
+++ env.tpl
@@ -2,22 +2,27 @@
 AIRFLOW_PROJ_HOST_DIR="{{ pwd }}/alertflow"
 AIRFLOW_VOLUMES_HOST_DIR="{{ pwd }}/volumes"
 AIRFLOW_IMAGE_NAME=apache/airflow:2.5.1
-AIRFLOW_UID="{{ id -u }}"
-AIRFLOW_GID="{{ id -g }}"
+HOST_UID="{{ id -u }}"
+HOST_GID="{{ id -g }}"
 
 # Web
 _AIRFLOW_WWW_USER_USERNAME=
 _AIRFLOW_WWW_USER_PASSWORD=
 
+# Email
+EMAIL_MAIN=
+
 # External Postgres Connection
 # https://airflow.apache.org/docs/apache-airflow/stable/howto/connection.html
-PSQL_USER=
-PSQL_PASSWORD=
-PSQL_HOST=
-PSQL_PORT=
-PSQL_DB=
-# Do not change
-AIRFLOW_CONN_POSTGRES_MAIN='postgresql://${PSQL_USER}:${PSQL_PASSWORD}@${PSQL_HOST}:${PSQL_PORT}/${PSQL_DB}'
+PSQL_USER_MAIN=
+PSQL_PASSWORD_MAIN=
+PSQL_HOST_MAIN=
+PSQL_PORT_MAIN=
+PSQL_DB_MAIN=
+
+# Satellite Weather (format: CDSAPI_KEY="UID:KEY")
+# https://cds.climate.copernicus.eu/user/{MY_USER}
+CDSAPI_KEY=
 
 # Extras
 AIRFLOW__CORE__FERNET_KEY=''
