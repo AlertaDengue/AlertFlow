091fc01f27dcd5be7ce02a9ef3c08bd7112b6260
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-12 19:31:38 -0300

    Fix foz dag

--- alertflow/dags/satellite-weather/foz_do_iguacu.py
+++ alertflow/dags/satellite-weather/foz_do_iguacu.py
@@ -1,20 +1,10 @@
-import calendar
 import os
-from datetime import datetime, timedelta
-from pathlib import Path, PosixPath
+from datetime import timedelta
 
 import pendulum
 from airflow import DAG
-from airflow.decorators.python import python_task
-from airflow.operators.python import PythonOperator
-from dateutil import parser
-from satellite import downloader as sat_d
-from satellite import weather as sat_w
-from sqlalchemy import create_engine
+from airflow.decorators import task
 
-env = os.getenv
-email_main = env('EMAIL_MAIN')
-DATA_DIR = '/tmp/copernicus/foz'
 DEFAULT_ARGS = {
     'owner': 'AlertaDengue',
     'depends_on_past': False,
@@ -25,6 +15,9 @@ DEFAULT_ARGS = {
     'retry_delay': timedelta(minutes=2),
 }
 
+env = os.getenv
+email_main = env('EMAIL_MAIN')
+DATA_DIR = '/tmp/copernicus'
 PG_URI_MAIN = (
     'postgresql://'
     f"{env('PSQL_USER_MAIN')}"
@@ -33,93 +26,79 @@ PG_URI_MAIN = (
     f":{env('PSQL_PORT_MAIN')}"
     f"/{env('PSQL_DB_MAIN')}"
 )
-TABLE_NAME = 'copernicus_foz_do_iguacu'
-SCHEMA = 'weather'
-
+CDSAPI_KEY = env('CDSAPI_KEY')
 
 with DAG(
-    dag_id='COPERNICUS_FOZ_DO_IGUACU',
-    description='ETL of weather data for Foz do Iguaçu',
+    dag_id='COPERNICUS_FOZ',
+    description='ETL of weather data for Foz do Iguaçu - BR',
     tags=['Brasil', 'Copernicus', 'Foz do Iguaçu'],
-    schedule='@monthly',
+    schedule='@weekly',
     default_args=DEFAULT_ARGS,
-    start_date=pendulum.datetime(2000, 2, 15),
-    catchup=True,
+    start_date=pendulum.datetime(2000, 1, 9),
+    catchup=False,  # TODO CHANGE TO TRUE
+    max_active_runs=10,
 ):
 
-    def download_netcdf(ini_date: str) -> PosixPath:
-        """
-        Downloads the file for current task execution
-        date - 1 month, extracting always the last month of
-        a given execution date. Returns the local NetCDF4
-        file to be inserted into postgres.
-        """
-        start_date = parser.parse(str(ini_date))
-        if start_date.month == 1:   # If January
-            last_month = datetime(
-                year=start_date.year - 1, month=12, day=start_date.day
-            )
-        else:
-            last_month = datetime(
-                year=start_date.year,
-                month=start_date.month - 1,
-                day=start_date.day,
-            )
+    DATE = '{{ ds }}'
 
-        ini_date = datetime(
-            year=last_month.year, month=last_month.month, day=1
-        ).date()
+    @task.external_python(
+        task_id='weekly_fetch', python='/opt/py310/bin/python3.10'
+    )
+    def extract_transform_load(
+        date: str, data_dir: str, api_key: str, psql_uri: str
+    ) -> None:
+        from datetime import timedelta
+        from pathlib import Path
 
-        _, last_month_day = calendar.monthrange(
-            year=last_month.year, month=last_month.month
-        )
-
-        end_date = datetime(
-            year=last_month.year, month=last_month.month, day=last_month_day
-        ).date()
+        from dateutil import parser
+        from satellite import downloader as sat_d
+        from satellite import weather as sat_w
+        from sqlalchemy import create_engine
 
         try:
-            netcdf_file = sat_d.download_br_netcdf(
-                date=str(ini_date), date_end=str(end_date), data_dir=DATA_DIR
-            )
-            filepath = Path(DATA_DIR) / netcdf_file
-            return str(filepath.absolute())
+            with create_engine(psql_uri).connect() as conn:
+                cur = conn.execute(
+                    'SELECT DISTINCT(date) '
+                    'FROM weather.copernicus_foz_do_iguacu'
+                )
+                dates = cur.all()
         except Exception as e:
-            raise e
+            if 'UndefinedTable' in str(e):
+                print('First insertion')
+                dates = []
+            else:
+                raise e
 
-    # https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html#variables
-    E = PythonOperator(
-        task_id='extract',
-        python_callable=download_netcdf,
-        op_kwargs={'ini_date': '{{ ds }}'},
-    )
+        if date in dates:
+            print(f'[INFO] {date} has been fetched already.')
+            return None
 
-    @python_task(task_id='loading')
-    def upload_dataset(**context) -> PosixPath:
-        """
-        Reads the NetCDF file and generate the dataframe for all
-        geocodes from IBGE using XArray and insert every geocode
-        into postgres database.
-        """
-        ti = context['ti']
-        file = ti.xcom_pull(task_ids='extract')
-        ds = sat_w.load_dataset(file)
-        df = ds.copebr.to_dataframe(geocodes=4108304, raw=True)
-        with create_engine(PG_URI_MAIN).connect() as conn:
+        start_date = parser.parse(str(date)).date()
+        max_update_delay = start_date - timedelta(days=9)
+        end_date = max_update_delay - timedelta(days=7)
+
+        netcdf_file = sat_d.download_br_netcdf(
+            date=str(max_update_delay),
+            date_end=str(end_date),
+            data_dir=data_dir,
+            user_key=api_key,
+        )
+        filepath = Path(data_dir) / netcdf_file
+
+        ds = sat_w.load_dataset(filepath)
+
+        df = ds.copebr.to_dataframe(4108304, raw=True)
+
+        with create_engine(psql_uri).connect() as conn:
             df.to_sql(
-                name=TABLE_NAME, schema=SCHEMA, con=conn, if_exists='append'
+                name='copernicus_foz_do_iguacu',
+                schema='weather',
+                con=conn,
+                if_exists='append',
             )
 
-    @python_task(task_id='clean')
-    def remove_netcdf(**context):
-        """Remove the file downloaded by extract task"""
-        ti = context['ti']
-        file = ti.xcom_pull(task_ids='extract')
-        Path(file).unlink(missing_ok=False)
+        Path(filepath).unlink(missing_ok=False)
 
-    # Creating the tasks
-    TL = upload_dataset()
-    clean = remove_netcdf()
+    ETL = extract_transform_load(DATE, DATA_DIR, CDSAPI_KEY, PG_URI_MAIN)
 
-    # Task flow
-    E >> TL >> clean
+    ETL
