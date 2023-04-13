6042da2c81738c2e32d1bceb9baa22df744edc5e
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-13 13:38:48 -0300

    Docstrings

--- alertflow/dags/satellite-weather/brasil.py
+++ alertflow/dags/satellite-weather/brasil.py
@@ -1,3 +1,21 @@
+"""
+ᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖ
+Author: Luã Bida Vacaro
+Email: luabidaa@gmail.com
+Github: https://github.com/luabida
+Date: 2023-04-13
+ᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖ
+
+The COPERNICUS_BRASIL Airflow DAG will collect daily weather
+data from the Copernicus ERA5 Reanalysis dataset for all 5570
+cities in Brazil. This data includes temperature, precipitation,
+humidity, and atmospheric pressure, which is collected daily
+starting from January 1st, 2000 to the present day.
+
+To ensure reliability and safety, the DAG has a 9-day delay
+from the current date, as the Copernicus API usually takes
+around 7 days to update the dataset.
+"""
 import os
 from datetime import timedelta
 
@@ -39,7 +57,7 @@ with DAG(
     max_active_runs=8,
 ):
 
-    DATE = '{{ ds }}'
+    DATE = '{{ ds }}'   # DAG execution date
 
     @task.external_python(
         task_id='daily_fetch', python='/opt/py310/bin/python3.10'
@@ -47,6 +65,20 @@ with DAG(
     def extract_transform_load(
         date: str, data_dir: str, api_key: str, psql_uri: str
     ) -> None:
+        """
+        Due to incompatibility issues between Airflow's Python version
+        and the satellite-weather-downloader (SWD) package, this task
+        will be executed in a dedicated virtual environment, which
+        includes a pre-installed Python3.10 interpreter within the
+        container. All imports must be within the scope of the task,
+        and XCom sharing between tasks is not allowed.
+
+        The task is designed to receive the execution date and download
+        the weather dataset for that specific day. After downloading,
+        the data is transformed using Xarray and inserted into the Main
+        Postgres DB, as specified in the .env file, in the form of a
+        DataFrame containing the weather information.
+        """
         from datetime import datetime, timedelta
         from itertools import chain
         from pathlib import Path
@@ -61,6 +93,7 @@ with DAG(
             return dt.strftime('%F')
 
         def is_date_in_db(date: str) -> bool:
+            # Checks if date has been already inserted into DB
             try:
                 with create_engine(psql_uri).connect() as conn:
                     cur = conn.execute(
@@ -70,6 +103,7 @@ with DAG(
                     dates = list(chain(*cur.all()))
             except Exception as e:
                 if 'UndefinedTable' in str(e):
+                    # For dev purposes, in case table was not found
                     print('First insertion')
                     dates = []
                 else:
@@ -86,17 +120,21 @@ with DAG(
             print(f'[INFO] {date} has been fetched already.')
             return None
 
+        # Downloads daily dataset
         netcdf_file = sat_d.download_br_netcdf(
             date=str(max_update_delay), data_dir=data_dir, user_key=api_key
         )
         filepath = Path(data_dir) / netcdf_file
         print(f'[INFO] Handling {filepath}')
 
+        # Reads the NetCDF4 file using Xarray
         ds = sat_w.load_dataset(filepath)
         geocodes = [mun['geocodigo'] for mun in MUNICIPIOS]
 
+        # Transforms the data into Pandas DataFrame
         df = ds.copebr.to_dataframe(geocodes, raw=False)
 
+        # Inserts weather data into DB
         with create_engine(psql_uri).connect() as conn:
             if is_date_in_db(date=format_date(max_update_delay)):
                 # Second assertion in case file is being handle twice
@@ -110,8 +148,10 @@ with DAG(
                 if_exists='append',
             )
 
+        # Deletes the NetCDF4 file
         Path(filepath).unlink(missing_ok=True)
 
+    # Instantiate the Task
     ETL = extract_transform_load(DATE, DATA_DIR, CDSAPI_KEY, PG_URI_MAIN)
 
-    ETL
+    ETL   # Execute
--- alertflow/dags/satellite-weather/foz_do_iguacu.py
+++ alertflow/dags/satellite-weather/foz_do_iguacu.py
@@ -1,3 +1,20 @@
+"""
+ᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖ
+Author: Luã Bida Vacaro
+Email: luabidaa@gmail.com
+Github: https://github.com/luabida
+Date: 2023-04-13
+ᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖᆖᆖᆖᆖᆖᆖᆖᆖλᆖ
+
+The COPERNICUS_FOZ Airflow DAG will retrieve weekly weather data
+for the Brazilian city of Foz do Iguaçu by accessing the Copernicus
+ERA5 Reanalysis dataset. This climate information includes temperature,
+precipitation, humidity, and atmospheric pressure, which is collected
+every 3 hours daily from January 1st, 2000 to the present. For safety
+measures, the DAG is programmed to have a 9-day delay from the current
+date, considering that the Copernicus API typically takes an average
+of 7 days to update the dataset.
+"""
 import os
 from datetime import timedelta
 
@@ -39,7 +56,7 @@ with DAG(
     max_active_runs=15,
 ):
 
-    DATE = '{{ ds }}'
+    DATE = '{{ ds }}'   # DAG execution date
 
     @task.external_python(
         task_id='weekly_fetch', python='/opt/py310/bin/python3.10'
@@ -47,6 +64,20 @@ with DAG(
     def extract_transform_load(
         date: str, data_dir: str, api_key: str, psql_uri: str
     ) -> None:
+        """
+        Due to incompatibility issues between Airflow's Python version
+        and the satellite-weather-downloader (SWD) package, this task
+        will be executed in a dedicated virtual environment, which
+        includes a pre-installed Python3.10 interpreter within the
+        container. All imports must be within the scope of the task,
+        and XCom sharing between tasks is not allowed.
+
+        The task is designed to receive the execution date and download
+        the weather dataset for that specific week range. After downloading,
+        the data is transformed using Xarray and inserted into the Main
+        Postgres DB, as specified in the .env file, in the form of a
+        DataFrame containing the weather information.
+        """
         from datetime import datetime, timedelta
         from itertools import chain
         from pathlib import Path
@@ -57,6 +88,7 @@ with DAG(
         from sqlalchemy import create_engine
 
         try:
+            # Check if date has been already inserted
             with create_engine(psql_uri).connect() as conn:
                 cur = conn.execute(
                     'SELECT DISTINCT(datetime::DATE) '
@@ -81,6 +113,7 @@ with DAG(
             print(f'[INFO] {date} has been fetched already.')
             return None
 
+        # Downloads the NetCDF4 dataset
         netcdf_file = sat_d.download_br_netcdf(
             date=str(start_date),
             date_end=str(max_update_delay),
@@ -89,10 +122,13 @@ with DAG(
         )
         filepath = Path(data_dir) / netcdf_file
 
+        # Reads the dataset
         ds = sat_w.load_dataset(filepath)
 
+        # Transform the data, returns a pandas DataFrame
         df = ds.copebr.to_dataframe(4108304, raw=True)
 
+        # Insert the DataFrame into DB
         with create_engine(psql_uri).connect() as conn:
             df.to_sql(
                 name='copernicus_foz_do_iguacu',
@@ -102,8 +138,10 @@ with DAG(
             )
         print(f'{filepath} inserted into weather.copernicus_foz_do_iguacu')
 
+        # Deletes the dataset
         Path(filepath).unlink(missing_ok=True)
 
+    # Instantiate the Task
     ETL = extract_transform_load(DATE, DATA_DIR, CDSAPI_KEY, PG_URI_MAIN)
 
-    ETL
+    ETL   # Execute
