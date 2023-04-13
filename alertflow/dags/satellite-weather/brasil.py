40f81097c3c7e4c9c2cc8e262ef10a576ef23a2f
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-13 11:19:17 -0300

    Fix brasil dag

--- alertflow/dags/satellite-weather/brasil.py
+++ alertflow/dags/satellite-weather/brasil.py
@@ -47,7 +47,8 @@ with DAG(
     def extract_transform_load(
         date: str, data_dir: str, api_key: str, psql_uri: str
     ) -> None:
-        from datetime import timedelta
+        from datetime import datetime, timedelta
+        from itertools import chain
         from pathlib import Path
 
         from dateutil import parser
@@ -56,30 +57,40 @@ with DAG(
         from satellite.weather._brazil.extract_latlons import MUNICIPIOS
         from sqlalchemy import create_engine
 
-        try:
-            with create_engine(psql_uri).connect() as conn:
-                cur = conn.execute(
-                    'SELECT DISTINCT(date) FROM weather.copernicus_brasil'
-                )
-                dates = cur.all()
-        except Exception as e:
-            if 'UndefinedTable' in str(e):
-                print('First insertion')
-                dates = []
-            else:
-                raise e
+        def format_date(dt: datetime):
+            return dt.strftime('%F')
 
-        if date in dates:
-            print(f'[INFO] {date} has been fetched already.')
-            return None
+        def is_date_in_db(date: str) -> bool:
+            try:
+                with create_engine(psql_uri).connect() as conn:
+                    cur = conn.execute(
+                        'SELECT DISTINCT(date::DATE)'
+                        ' FROM weather.copernicus_brasil'
+                    )
+                    dates = list(chain(*cur.all()))
+            except Exception as e:
+                if 'UndefinedTable' in str(e):
+                    print('First insertion')
+                    dates = []
+                else:
+                    raise e
+
+            if str(date) in list(map(format_date, dates)):
+                return True
+            return False
 
         start_date = parser.parse(str(date)).date()
         max_update_delay = start_date - timedelta(days=9)
+
+        if is_date_in_db(date=format_date(max_update_delay)):
+            print(f'[INFO] {date} has been fetched already.')
+            return None
 
         netcdf_file = sat_d.download_br_netcdf(
             date=str(max_update_delay), data_dir=data_dir, user_key=api_key
         )
         filepath = Path(data_dir) / netcdf_file
+        print(f'[INFO] Handling {filepath}')
 
         ds = sat_w.load_dataset(filepath)
         geocodes = [mun['geocodigo'] for mun in MUNICIPIOS]
@@ -87,6 +98,11 @@ with DAG(
         df = ds.copebr.to_dataframe(geocodes, raw=False)
 
         with create_engine(psql_uri).connect() as conn:
+            if is_date_in_db(date=format_date(max_update_delay)):
+                # Second assertion in case file is being handle twice
+                print(f'[INFO] {date} has been fetched already.')
+                return None
+
             df.to_sql(
                 name='copernicus_brasil',
                 schema='weather',
@@ -94,7 +110,7 @@ with DAG(
                 if_exists='append',
             )
 
-        Path(filepath).unlink(missing_ok=False)
+        Path(filepath).unlink(missing_ok=True)
 
     ETL = extract_transform_load(DATE, DATA_DIR, CDSAPI_KEY, PG_URI_MAIN)
 
