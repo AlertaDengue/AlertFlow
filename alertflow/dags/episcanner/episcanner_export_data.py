1bca3f4ed06cb2a9ed481f5702d5c8c4eb5ba370
Luã Bida Vacaro <luabidaa@gmail.com>
2024-02-20 13:39:26 -0300

    fix linter

--- alertflow/dags/episcanner/episcanner_export_data.py
+++ alertflow/dags/episcanner/episcanner_export_data.py
@@ -13,84 +13,84 @@ def set_airflow_variables():
     Set Airflow variables from environment and write them to the .env file.
     """
     # Set Airflow variables from environment variables
-    PSQL_USER = os.environ.get('AIRFLOW_PSQL_USER_MAIN')
-    PSQL_PASSWORD = os.environ.get('AIRFLOW_PSQL_PASSWORD_MAIN')
-    PSQL_HOST = os.environ.get('AIRFLOW_PSQL_HOST_MAIN')
-    PSQL_PORT = os.environ.get('AIRFLOW_PSQL_PORT_MAIN')
-    PSQL_DB = os.environ.get('AIRFLOW_PSQL_DB_MAIN')
+    PSQL_USER = os.environ.get("AIRFLOW_PSQL_USER_MAIN")
+    PSQL_PASSWORD = os.environ.get("AIRFLOW_PSQL_PASSWORD_MAIN")
+    PSQL_HOST = os.environ.get("AIRFLOW_PSQL_HOST_MAIN")
+    PSQL_PORT = os.environ.get("AIRFLOW_PSQL_PORT_MAIN")
+    PSQL_DB = os.environ.get("AIRFLOW_PSQL_DB_MAIN")
 
-    Variable.set('PSQL_USER', PSQL_USER)
-    Variable.set('PSQL_PASSWORD', PSQL_PASSWORD)
-    Variable.set('PSQL_HOST', PSQL_HOST)
-    Variable.set('PSQL_PORT', PSQL_PORT)
-    Variable.set('PSQL_DB', PSQL_DB)
+    Variable.set("PSQL_USER", PSQL_USER)
+    Variable.set("PSQL_PASSWORD", PSQL_PASSWORD)
+    Variable.set("PSQL_HOST", PSQL_HOST)
+    Variable.set("PSQL_PORT", PSQL_PORT)
+    Variable.set("PSQL_DB", PSQL_DB)
 
     # Write variables to .env file
-    dotenv_path = '/opt/airflow/episcanner-downloader/.env'
+    dotenv_path = "/opt/airflow/episcanner-downloader/.env"
     env_vars = dotenv_values(dotenv_path)
-    env_vars['PSQL_USER'] = PSQL_USER
-    env_vars['PSQL_PASSWORD'] = PSQL_PASSWORD
-    env_vars['PSQL_HOST'] = PSQL_HOST
-    env_vars['PSQL_PORT'] = PSQL_PORT
-    env_vars['PSQL_DB'] = PSQL_DB
+    env_vars["PSQL_USER"] = PSQL_USER
+    env_vars["PSQL_PASSWORD"] = PSQL_PASSWORD
+    env_vars["PSQL_HOST"] = PSQL_HOST
+    env_vars["PSQL_PORT"] = PSQL_PORT
+    env_vars["PSQL_DB"] = PSQL_DB
     for key, value in env_vars.items():
         set_key(dotenv_path, key, value)
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 5, 21),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 5, 21),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 with DAG(
-    'EPISCANNER_DOWNLOADER',
+    "EPISCANNER_DOWNLOADER",
     default_args=default_args,
-    schedule_interval='0 3 * * 0',  # Every Sunday at 3 AM
+    schedule_interval="0 3 * * 0",  # Every Sunday at 3 AM
     catchup=False,
 ) as dag:
 
     # clone the repository from GitHub
     clone_repository = BashOperator(
-        task_id='clone_repository',
-        bash_command='git clone --branch main --single-branch --depth 1 '
-        'https://github.com/AlertaDengue/episcanner-downloader.git '
-        '/opt/airflow/episcanner-downloader',
+        task_id="clone_repository",
+        bash_command="git clone --branch main --single-branch --depth 1 "
+        "https://github.com/AlertaDengue/episcanner-downloader.git "
+        "/opt/airflow/episcanner-downloader",
         dag=dag,
     )
 
     # Set variables for Episcanner-PostgreSQL connection
     set_connection_variables = PythonOperator(
-        task_id='set_connection_variables',
+        task_id="set_connection_variables",
         python_callable=set_airflow_variables,
         dag=dag,
     )
 
     # Install the Episcanner package using Poetry
     install_episcanner = BashOperator(
-        task_id='install_episcanner',
-        bash_command='source /home/airflow/mambaforge/bin/activate episcanner-downloader && '  # NOQA E501
-        'cd /opt/airflow/episcanner-downloader && '
-        'poetry install',
+        task_id="install_episcanner",
+        bash_command="source /home/airflow/mambaforge/bin/activate episcanner-downloader && "  # NOQA E501
+        "cd /opt/airflow/episcanner-downloader && "
+        "poetry install",
         dag=dag,
     )
 
     # Download all data to the specified directory
     episcanner_downloader = BashOperator(
-        task_id='episcanner_downloader',
-        bash_command='source /home/airflow/mambaforge/bin/activate episcanner-downloader && '  # NOQA E501
-        'cd /opt/airflow/episcanner-downloader &&'
-        'python epi_scanner/downloader/export_data.py '
-        '-s all -d dengue chikungunya -o /opt/airflow/episcanner_data',
+        task_id="episcanner_downloader",
+        bash_command="source /home/airflow/mambaforge/bin/activate episcanner-downloader && "  # NOQA E501
+        "cd /opt/airflow/episcanner-downloader &&"
+        "python epi_scanner/downloader/export_data.py "
+        "-s all -d dengue chikungunya -o /opt/airflow/episcanner_data",
         dag=dag,
     )
 
     # Remove the episcanner-downloader repository
     remove_repository = BashOperator(
-        task_id='remove_repository',
-        bash_command='rm -rf /opt/airflow/episcanner-downloader',
+        task_id="remove_repository",
+        bash_command="rm -rf /opt/airflow/episcanner-downloader",
         dag=dag,
     )
 
--- alertflow/dags/satellite-weather/brasil.py
+++ alertflow/dags/satellite-weather/brasil.py
@@ -14,6 +14,7 @@ To ensure reliability and safety, the DAG has a 9-day delay
 from the current date, as the Copernicus API usually takes
 around 7 days to update the dataset.
 """
+
 import os
 from datetime import timedelta
 
@@ -22,24 +23,24 @@ from airflow import DAG
 from airflow.decorators import task
 
 env = os.getenv
-email_main = env('EMAIL_MAIN')
+email_main = env("EMAIL_MAIN")
 
 DEFAULT_ARGS = {
-    'owner': 'AlertaDengue',
-    'depends_on_past': False,
+    "owner": "AlertaDengue",
+    "depends_on_past": False,
     # 'email': [email_main],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=2),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=2),
 }
 
 
 with DAG(
-    dag_id='COPERNICUS_BRASIL',
-    description='ETL of weather data for Brazil',
-    tags=['Brasil', 'Copernicus'],
-    schedule='@daily',
+    dag_id="COPERNICUS_BRASIL",
+    description="ETL of weather data for Brazil",
+    tags=["Brasil", "Copernicus"],
+    schedule="@daily",
     default_args=DEFAULT_ARGS,
     start_date=pendulum.datetime(2014, 1, 1),
     # catchup=True,
@@ -48,14 +49,17 @@ with DAG(
 ):
     from airflow.models import Variable
 
-    DATE = '{{ ds }}'   # DAG execution date
-    DATA_DIR = '/tmp/copernicus'
-    KEY = Variable.get('cdsapi_key', deserialize_json=True)
-    URI = Variable.get('psql_main_uri', deserialize_json=True)
+    DATE = "{{ ds }}"  # DAG execution date
+    DATA_DIR = "/tmp/copernicus"
+    KEY = Variable.get("cdsapi_key", deserialize_json=True)
+    URI = Variable.get("psql_main_uri", deserialize_json=True)
 
+    # fmt: off
     @task.external_python(
-        task_id='daily_fetch', python='/opt/py310/bin/python3.10'
+        task_id="daily_fetch",
+        python="/opt/py310/bin/python3.10"
     )
+    # fmt: on
     def extract_transform_load(
         date: str, data_dir: str, api_key: str, psql_uri: str
     ) -> str:
@@ -86,37 +90,37 @@ with DAG(
         start_date = parser.parse(str(date))
         max_update_delay = start_date - timedelta(days=8)
 
-        with create_engine(psql_uri['PSQL_MAIN_URI']).connect() as conn:
+        with create_engine(psql_uri["PSQL_MAIN_URI"]).connect() as conn:
             cur = conn.execute(
-                'SELECT geocodigo FROM weather.copernicus_brasil'
+                "SELECT geocodigo FROM weather.copernicus_brasil"
                 f" WHERE date = '{str(max_update_delay.date())}'"
             )
             table_geocodes = set(chain(*cur.fetchall()))
 
-        all_geocodes = set([mun['geocodigo'] for mun in MUNICIPALITIES])
+        all_geocodes = set([mun["geocodigo"] for mun in MUNICIPALITIES])
         geocodes = all_geocodes.difference(table_geocodes)
-        print('TABLE_GEO ', f'[{len(table_geocodes)}]: ', table_geocodes)
-        print('DIFF_GEO: ', f'[{len(geocodes)}]: ', geocodes)
+        print("TABLE_GEO ", f"[{len(table_geocodes)}]: ", table_geocodes)
+        print("DIFF_GEO: ", f"[{len(geocodes)}]: ", geocodes)
 
         if not geocodes:
-            return 'There is no geocode to fetch'
+            return "There is no geocode to fetch"
 
         # Downloads daily dataset
         netcdf_file = sat_d.download_br_netcdf(
             date=str(max_update_delay.date()),
             data_dir=data_dir,
-            user_key=api_key['CDSAPI_KEY'],
+            user_key=api_key["CDSAPI_KEY"],
         )
 
-        print(f'Handling {netcdf_file}')
+        print(f"Handling {netcdf_file}")
 
         # Reads the NetCDF4 file using Xarray
         ds = sat_w.load_dataset(netcdf_file)
 
-        with create_engine(psql_uri['PSQL_MAIN_URI']).connect() as conn:
+        with create_engine(psql_uri["PSQL_MAIN_URI"]).connect() as conn:
             ds.copebr.to_sql(
-                tablename='copernicus_brasil',
-                schema='weather',
+                tablename="copernicus_brasil",
+                schema="weather",
                 geocodes=list(geocodes),
                 con=conn,
             )
@@ -124,9 +128,9 @@ with DAG(
         # Deletes the NetCDF4 file
         Path(netcdf_file).unlink(missing_ok=True)
 
-        return f'{len(geocodes)} inserted into DB.'
+        return f"{len(geocodes)} inserted into DB."
 
     # Instantiate the Task
     ETL = extract_transform_load(DATE, DATA_DIR, KEY, URI)
 
-    ETL   # Execute
+    ETL  # Execute
